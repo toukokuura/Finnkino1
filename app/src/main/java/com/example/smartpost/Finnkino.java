e333c1c5f118fac8ff89b3f5a3390fc57b1b9a2d
tikka <tikkanen.kuura@gmail.com>
2021-03-19 17:17:24 +0200

    tehtävä 9.3 lisäykset

--- app/src/main/java/com/example/smartpost/Finnkino.java
+++ app/src/main/java/com/example/smartpost/Finnkino.java
@@ -25,8 +25,10 @@ public class Finnkino {
 
     }
 
+    //--------------------------------------------------------------------------------
+
     public void addTeatteri(Element e) {
-        // lisätään smartpost-olioita XML-tiedostosta
+        // lisätään teatteriolio XML-tiedostosta
         teatteri_array.add(new Teatteri(
                 e.getElementsByTagName("Name").item(0).getTextContent(),
                 e.getElementsByTagName("ID").item(0).getTextContent()));
@@ -39,13 +41,58 @@ public class Finnkino {
 
             // account i from array equal to accNUmber
             if (teatteri_array.get(i).getNimi().compareTo(name) == 0) {
-                // print account:
                 return (getInfo(i));
             }
         }
 
         return("Ei teattereita");
     }
+
+    //--------------------------------------------------------------------------------
+
+    public String returnTeatteriID(String name) {
+        // returns the theaterID based on name
+
+        for (int i=0; i<teatteri_array.size(); i++) {
+
+            // account i from array equal to accNUmber
+            if (teatteri_array.get(i).getNimi().compareTo(name) == 0) {
+                return (teatteri_array.get(i).getID());
+            }
+        }
+
+        return("0000");
+    }
+
+    //--------------------------------------------------------------------------------
+
+    public void addMovies(Element e, String theaterID, String date) {
+        //etsi oikea teatteri
+        int i = findTeatteriID(theaterID);
+
+        if (i>-1) {
+            //lisätään elokuvat tiettyyn teatteriin
+            teatteri_array.get(i).addMovies(e);
+        }
+    }
+
+
+    public int findTeatteriID(String theaterID) {
+        // !!!! remember exception handling if calling this function !!!!
+        // returns place of theater in theater array based on ID
+        int i;
+        for (i=0; i<teatteri_array.size(); i++) {
+
+            if (teatteri_array.get(i).getID().compareTo(theaterID) == 0) {
+                return (i);
+            }
+        }
+
+        System.out.println("No theatre selected");
+        return i=-1;
+    }
+
+    //--------------------------------------------------------------------------------
 
     public ArrayList<String> getAll() {
         // return all teatteri entities as string array
@@ -63,6 +110,32 @@ public class Finnkino {
 
         return ("Teatteri: " + teatteri_array.get(i).getNimi()
                 + "\nID: " + teatteri_array.get(i).getID());
+    }
+
+    public ArrayList<String> getMovieInfo(String theaterID) {
+        // return all movies from a specific theater
+
+        int i = findTeatteriID(theaterID);
+
+        // change format to string
+        ArrayList<String> movies = new ArrayList<String>();
+
+        if (i==-1) {
+            return movies;
+        } else {
+            ArrayList<Elokuva> elokuva_array = teatteri_array.get(i).getInfo();
+
+
+
+            for (int j = 0; j < elokuva_array.size(); j++) {
+
+                movies.add(elokuva_array.get(j).getTitle()
+                        + " (" + elokuva_array.get(j).getProductionYear() + ") \nPituus: "
+                        + elokuva_array.get(j).getLengthMin() + "min \nIkäraja: "
+                        + elokuva_array.get(j).getRating());
+            }
+            return movies;
+        }
     }
 
     public ArrayList<String> getNames () {
--- app/src/main/java/com/example/smartpost/Teatteri.java
+++ app/src/main/java/com/example/smartpost/Teatteri.java
@@ -1,14 +1,38 @@
 package com.example.smartpost;
 
+import org.w3c.dom.Element;
+
+import java.util.ArrayList;
+
 public class Teatteri {
 
-    String nimi = "";
-    String id = "";
+    private String nimi = "";
+    private String id = "";
+    private ArrayList<Elokuva> elokuva_array = new ArrayList<Elokuva>();
 
     // annetut nimet
     public Teatteri(String a, String b){
         nimi = a;
         id = b;
+    }
+
+    //--------------------------------------------------------------------------------
+
+    public void addMovies(Element e){
+        elokuva_array.add(new Elokuva(
+                e.getElementsByTagName("ID").item(0).getTextContent(),
+                e.getElementsByTagName("Title").item(0).getTextContent(),
+                e.getElementsByTagName("ProductionYear").item(0).getTextContent(),
+                e.getElementsByTagName("LengthInMinutes").item(0).getTextContent(),
+                e.getElementsByTagName("Rating").item(0).getTextContent()
+        ));
+    }
+
+    //--------------------------------------------------------------------------------
+
+    public ArrayList<Elokuva> getInfo() {
+        // return all info of a elokuva entities
+        return elokuva_array;
     }
 
     public String getNimi() {
