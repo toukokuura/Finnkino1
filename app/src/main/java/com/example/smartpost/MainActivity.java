8ffe240ac179a9b3e7f3e5ac33c9a5c0914c0d1c
tikka <tikkanen.kuura@gmail.com>
2021-03-19 17:15:29 +0200

    tehtävä 9.3 lisäykset

--- app/src/main/java/com/example/smartpost/MainActivity.java
+++ app/src/main/java/com/example/smartpost/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.smartpost;
 // https://demonuts.com/android-spinner-searchable/
 // Touko Tikkanen 1.4.2020
+// updated 22.3.2021
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,8 +25,10 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -45,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
     // Creation of singleton
     Finnkino list = Finnkino.getInstance();
 
-    ArrayList<String> options = new ArrayList<String>();
-    ArrayList<String> optionNames = new ArrayList<String>();
+    ArrayList<String> movies = null;
+    ArrayList<String> theaters = new ArrayList<String>();
     ArrayList<String> filterlist;
 
     @Override
@@ -66,15 +69,18 @@ public class MainActivity extends AppCompatActivity {
 
         // read Finnkino information from website when opening app
         readXML();
+
         // add this information to an arraylist
-        options = list.getAll();
-        optionNames = list.getNames(); // names used for spinner
+        //options = list.getAll(); // this would add theater details to listview
+        theaters = list.getNames(); // names used for spinner
 
+        //--------------------------------------------------------------------------------
 
-        // adapter for spinner and listview
+        // adapter for spinner
         // https://stackoverflow.com/questions/19820803/android-spinner-using-arrayliststring
         // https://www.codeproject.com/Tips/894233/Using-Spinner-Control-for-Filtering-ListView-Andro
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, optionNames);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, theaters);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
 
@@ -82,7 +88,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 name = parent.getItemAtPosition(position).toString();
+
                 showSelected();
+
+                // add movies in the theater to listview
+                readmovieXML(list.returnTeatteriID(name));
+                System.out.println("Movies added!");
+
             }
 
             @Override
@@ -91,9 +103,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options);
-        listView.setAdapter(adapter);
 
+        //--------------------------------------------------------------------------------
 
         // filtering: https://stackoverflow.com/questions/36169944/adding-filter-for-spinner-and-filter-the-data-on-spinner-itself
         search.addTextChangedListener(new TextWatcher() {
@@ -107,14 +118,17 @@ public class MainActivity extends AppCompatActivity {
                     filterlist = new ArrayList<String>();
                     filterlist = getFilter(s.toString());
 
-                    //start using filtered list
+                    /*
+                    //start using filtered list in spinner
                     ArrayAdapter<String> modAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, filterlist);
                     listView.setAdapter(modAdapter);
                     modAdapter.notifyDataSetChanged();
 
+                     */
+
                 } else {
                     // search is empty -> use full option list
-                    listView.setAdapter(adapter);
+                    //listView.setAdapter(adapter);
                     //adapter.notifyDataSetChanged();
                 }
 
@@ -122,12 +136,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //--------------------------------------------------------------------------------------------
+
     public ArrayList<String> getFilter(CharSequence charSequence) {
         ArrayList<String> filterResultsData = new ArrayList<String>();;
         if(charSequence == null || charSequence.length() == 0) {
             return null;
         } else {
-            for(String data : options) {
+            for(String data : movies) {
                 // options contains a String list of all entity information, one entity per cell
                 // add in anything that matches the string written in search
 
@@ -142,25 +158,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void showSelected() {
         //  kirjoitetaan tiedot vastaavasta spinnerin oliosta
-        info.setText(list.findTeatteri(name));
+        //info.setText(list.findTeatteri(name));
     }
 
     public void readXML() {
         try {
             DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             String url1 = "https://www.finnkino.fi/xml/TheatreAreas/";
-            //String url2 = "https://iseteenindus.smartpost.ee/api/?request=destinations&country=EE&type=APT";
 
             Document doc1 = builder.parse(url1);
-           // Document doc2 = builder.parse(url2);
 
             System.out.println(doc1.getDocumentElement().getNodeName());
 
             NodeList nList = doc1.getDocumentElement().getElementsByTagName("TheatreArea");
             parseNode(nList);
 
-            //nList = doc2.getDocumentElement().getElementsByTagName("item");
-            //parseNode(nList);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -170,6 +182,40 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    public void readmovieXML(String theaterID) {
+        try {
+            DocumentBuilder builder2 = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+            // get current date for url
+            String date = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
+
+            String url = "https://www.finnkino.fi/xml/Schedule/?area=" + theaterID + "&dt=" + date;
+            System.out.println(url);
+
+            Document doc = builder2.parse(url);
+
+            System.out.println(doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getDocumentElement().getElementsByTagName("Show");
+
+            parseNodeMovie(nList, theaterID, date);
+            System.out.println("Movies retrieved. Adding...");
+
+            // add parsed movies to listview
+            movies = list.getMovieInfo(theaterID);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movies);
+            listView.setAdapter(adapter);
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
     private void parseNode(NodeList nList) {
@@ -182,6 +228,21 @@ public class MainActivity extends AppCompatActivity {
                 Element element = (Element) node;
 
                 list.addTeatteri(element);
+            }
+        }
+    }
+
+    private void parseNodeMovie(NodeList nList, String x, String y) {
+        for (int i=0; i<nList.getLength(); i++) {
+            Node node = nList.item(i);
+            //System.out.println(node.getNodeName());
+
+            if (node.getNodeType() == node.ELEMENT_NODE) {
+                // tehdään elementistä olio
+                Element element = (Element) node;
+
+                System.out.println("Adding movies...");
+                list.addMovies(element, x, y);
             }
         }
     }
